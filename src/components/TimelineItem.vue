4419fd4ec0dd0a905c0a383aa9d1ff8bf1932dec
Angelin Calu <angelin.calu@gmail.com>
2019-07-23 13:54:32 +0300

    Configured moment.js

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -2,14 +2,16 @@
   <b-row no-gutters class="justify-content-end justify-content-md-around align-items-start timeline-nodes">
     <b-col col="10" md="5" order="3" order-md="1" class="timeline-content">
       <h3 class="text-light" v-html="itemTimeline.title"></h3>
-      <h2 class="text-light" v-html="itemTimeline.subtitle"></h2>
+      <h2 v-html="itemTimeline.subtitle"></h2>
       <p v-html="itemTimeline.content"></p>
     </b-col>
-    <b-col col="2" sm="1" order="2" class="px-md-3 timeline-image text-md-center">
+    <b-col col="2" sm="1" order="2" class="timeline-image text-md-center">
       <img v-bind:src="'img/' + itemTimeline.image" class="img-fluid" :alt="itemTimeline.title">
     </b-col>
     <b-col col="10" md="5" order="1" order-md="3" class="py-3 timeline-date">
-      <time v-html="itemTimeline.formatedTime"></time>
+      {{ itemTimeline.from | FormatedDate }} - {{ itemTimeline.to | FormatedDate }}
+      <time v-html="TimeDifference(itemTimeline.from, itemTimeline.to)">
+      </time>
     </b-col>
   </b-row>
 </template>
@@ -27,19 +29,53 @@ export default {
       default: () => ({})
     }
   },
-  computed: {
-    formatedTime() {
 
-      let start = moment(this.itemTimeline.to);
-      let end = moment(this.itemTimeline.from);
+  methods: {
+    TimeDifference(from, to) {
 
-      var years = start.diff(end, 'year');
-      var months = start.diff(end, 'months');
+      let formatedTime = '<span class="duration">';
 
+      var yearsDiff = moment(to).diff(moment(from), 'year');
+      var monthsDiff = moment(to).diff(moment(from), 'months') - yearsDiff*12; //substract the full years as months
 
-      return years + "year(s) " + months + " month(s)";
+      let separator = '';
+
+      if (yearsDiff > 0 && monthsDiff > 0) {
+        separator = ', ';
+      }
+
+      // Show years only if the duration is over one year and handle the pluralisation
+      if (yearsDiff === 1) {
+        formatedTime = formatedTime + ' 1 year';
+      }else if (yearsDiff > 1) {
+        formatedTime = formatedTime + yearsDiff + ' years';
+      }
+
+      // Show months only if the months are over 1 and handle the pluralisation
+      if (monthsDiff === 1) {
+        formatedTime = formatedTime + separator + '1 month';
+      }else if (monthsDiff > 1) {
+        formatedTime = formatedTime + separator + monthsDiff + ' months';
+      }
+      
+      formatedTime = formatedTime + '</span>';
+
+      return formatedTime 
+    },
+  },
+
+  filters: {
+      FormatedDate: function (InputDate) {
+        let momentDate = moment(InputDate);
+
+        if (moment().isSame(momentDate, 'month')) {
+          return "Present time";
+        }else {
+          return momentDate.format("MM YYYY");
+        }
+      }
     }
-  }
 }
+
 
 </script>
