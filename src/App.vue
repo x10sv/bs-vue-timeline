5573fe33ff059c9cac7d07afc35141cd6e60260f
Angelin Calu <angelin.calu@gmail.com>
2019-07-23 15:36:19 +0300

    Edit example content

--- src/App.vue
+++ src/App.vue
@@ -18,25 +18,25 @@ export default {
       {
         from: new Date(2014,10),
         to: new Date(),
-        title: 'Microsoft',
+        title: 'Microsoft Corporation',
         subtitle: 'System Engineer',
-        content: 'lorem ipsum dolor sin amet',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet luctus venenatis lectus magna fringilla. Convallis convallis tellus id interdum velit laoreet id donec ultrices. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Aliquet enim tortor at auctor urna nunc id cursus metus. Interdum varius sit amet mattis vulputate enim nulla aliquet. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc. Morbi enim nunc faucibus a pellentesque sit amet porttitor.',
         image: 'microsoft.png'
       },
         {
         from: new Date(2009,5),
         to: new Date(2013,5),
-        title: 'Oracle',
+        title: 'Oracle Corporation',
         subtitle: 'Programmer',
-        content: 'lorem ipsum dolor sin amet',
+        content: 'Volutpat blandit aliquam etiam erat velit. Est pellentesque elit ullamcorper dignissim cras tincidunt. Morbi non arcu risus quis varius quam quisque. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis.',
         image: 'oracle.png'
       },
       {
         from: new Date(2000,3),
         to: new Date(2008,12),
-        title: 'Google Inc.',
+        title: 'Google LLC',
         subtitle: 'Internship / Volunteer',
-        content: 'lorem ipsum dolor sin amet',
+        content: 'Proin fermentum leo vel orci porta non pulvinar neque. Enim neque volutpat ac tincidunt vitae semper quis. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Cursus turpis massa tincidunt dui ut ornare. Enim nunc faucibus a pellentesque. Erat imperdiet sed euismod nisi porta lorem mollis. Tristique senectus et netus et malesuada fames ac turpis egestas. Magnis dis parturient montes nascetur ridiculus mus mauris. Fusce id velit ut tortor pretium.',
         image: 'google.png'
       },
     ]
